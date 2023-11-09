5c93f53bde2af153f06c3d6fa99bb07ce533a00a
zsfer <seiferalbacete@gmail.com>
2023-11-09 22:03:27 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,55 @@
+let foldIdx = 0;
+let planesFolded = 0;
+let canFold = true;
+let offscreen = false;
+
+let throwSpeed = 1000;
+
+$(() => {
+   updatePlane();
+
+   $(document).click(update);
+   $(document).keydown((e) => {
+      if (e.which === 32) {
+         update();
+      }
+   });
+});
+
+const update = () => {
+   if (!canFold) return;
+   foldIdx = (foldIdx + 1) % 5;
+   updatePlane();
+
+   updateHud();
+};
+
+const updatePlane = () => {
+   if (foldIdx === 4) {
+      canFold = false;
+      $(".plane__container").animate({ top: -800 }, throwSpeed, () => {
+         canFold = true;
+         offscreen = true;
+         foldIdx = 0;
+      });
+      planesFolded++;
+      return;
+   }
+
+   if (offscreen) {
+      foldIdx = 0;
+      canFold = false;
+      $(".plane__container").animate({ top: "50%" }, throwSpeed, () => {
+         canFold = true;
+         offscreen = false;
+      });
+   }
+
+   $(".plane__container").html(
+      `<img src="/assets/images/plane/${foldIdx + 1}.png"/>`
+   );
+};
+
+const updateHud = () => {
+   $("#hud__planes-folded h1").text(planesFolded);
+};
